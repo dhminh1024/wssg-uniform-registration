d9d4cd192965d017f3ae37c513d5afa9e29123ec
Minh Do <dhminh1024@gmail.com>
2024-09-25 09:56:44 +0700

    feat: add instructions for size

--- frontend/src/app/home/components/instructions.tsx
+++ frontend/src/app/home/components/instructions.tsx
@@ -35,6 +35,17 @@ export const Instructions: FC<InstructionsProps> = () => {
             {t("Instructions Step 3")}{" "}
             <EditIcon className="h-4 w-4 inline text-orange-500" />
           </li>
+          <li className="text-sm">
+            {t("Instructions Step 5")}
+            {": "}
+            <a
+              href="https://wellspringedu-my.sharepoint.com/:x:/g/personal/huong_lethithu_wellspring_edu_vn/EQMpdBd-5AZAo7wsCW9AiDsBhC0J-ytQKlE0s3qk52JIzg?e=tVo6k9"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Link
+            </a>
+          </li>
           {!allowRegistration && (
             <li className="text-sm font-semibold text-red-500">
               {t("Registration has been disabled")}
